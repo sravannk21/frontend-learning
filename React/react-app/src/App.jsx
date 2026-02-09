c85e23d3c2cc06b126630b3f58ce74823d3ae929
sravannk21 <yenkeyyes4@gmail.com>
2026-02-09 19:15:11 +0530

    Add gitignore for node modules

---
+++ React/react-app/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div>
+      <h1>Hello Shravan</h1>
+      <p>This is my first React component.</p>
+      <p>I am becoming a frontend developer.</p>
+    </div>
+  );
+}
+
+export default App;
