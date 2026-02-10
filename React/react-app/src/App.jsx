975536e2200dccff57b8f21e54a8181259d3c310
sravannk21 <yenkeyyes4@gmail.com>
2026-02-10 20:30:20 +0530

    Added reusable Card component with props and conditional rendering

--- React/react-app/src/App.jsx
+++ React/react-app/src/App.jsx
@@ -1,8 +1,9 @@
 function Card(props){
   return (
-    <div>
+    <div style={{border:"1px solid gray", padding:"10px", margin:"10px"}}>
       <h3>{props.name}</h3>
       <p>{props.role}</p>
+      {props.active ? <span>Active</span>: <span>Offline</span>}
     </div>
   )
 }
@@ -10,8 +11,8 @@ function Card(props){
 export default function App() {
   return (
     <div>
-      <Card name="Sravan NK" role="Frontend Developer" />
-      <Card name="Laika" role="Your guide" />
+      <Card name="Sravan NK" role="Frontend Developer" active={true} />
+      <Card name="Laika" role="Your guide" active={false}/>
     </div>
   )
 }
